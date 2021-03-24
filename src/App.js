5dea6e8f5effc957c3fc03f055fb846634722139
丸山司 <tsukasa@maruyamatsukasanoMac-mini.local>
2021-03-24 20:16:43 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-function Cat() {
-  return (<p>Meow!</p>)
-
+function User(props) {
+  return (<p>I am {props.name} and {props.age} years old.</p>)
 }
 
+User.defaultProps = {
+  age: 1,
+  name: "Noname"
+}
 
 function App() {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {}
+  ]
+
   return (
     <React.Fragment>
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+            return <User key={index} name={profile.name} age={profile.age} />
+        })
+      }
     </React.Fragment>
   );
-
 }
 
 export default App;
