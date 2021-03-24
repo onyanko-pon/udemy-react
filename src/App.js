d56e700d95f8027d04046836d6740d18b28b8715
丸山司 <tsukasa@maruyamatsukasanoMac-mini.local>
2021-03-24 20:46:29 +0900

    learn prop types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 function User(props) {
   return (<p>I am {props.name} and {props.age} years old.</p>)
 }
 
-User.defaultProps = {
-  age: 1,
-  name: "Noname"
+// User.defaultProps = {
+//   age: 1,
+//   name: "Noname"
+// }
+
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 
 function App() {
   const profiles = [
     {name: "Taro", age: 10},
     {name: "Hanako", age: 5},
-    {}
+    {name: "tsukasa", age: 23}
   ]
 
   return (
